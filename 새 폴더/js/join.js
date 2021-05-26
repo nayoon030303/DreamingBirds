f42a7aa624db382e220ecce4857c5ae52ce19161
nayoon030303 <nayoon030303@gmail.com>
2021-05-26 23:28:44 +0900

    alert items all

--- 새 폴더/js/join.js
+++ 새 폴더/js/join.js
@@ -6,7 +6,7 @@ function currentList(){
 
 function checkUserId(form){
     if(form.userId.value ==""){
-        document.querySelector("#alert_id").innerHTML = "Please enter id";
+        document.querySelector("#alert_id").innerHTML = "필수 정보입니다.";
         return false;
     }
     return true;
@@ -14,7 +14,7 @@ function checkUserId(form){
 
 function checkUserPassword(form){
     if(form.userPassword.value ==""){
-        document.querySelector("#alert_password").innerHTML = "Please enter password";
+        document.querySelector("#alert_password").innerHTML = "필수 정보입니다.";
         return false;
     }
     return true;
@@ -22,7 +22,7 @@ function checkUserPassword(form){
 
 function recheckUserPassword(form){
     if(form.userRepassword.value ==""){
-        document.querySelector("#alert_repassword").innerHTML = "Please enter password";
+        document.querySelector("#alert_repassword").innerHTML = "필수 정보입니다.";
         return false;
     }
     return true;
@@ -30,24 +30,51 @@ function recheckUserPassword(form){
 
 function checkUserName(form){
     if(form.userName.value ==""){
-        document.querySelector("#alert_name").innerHTML = "Please enter name";
+        document.querySelector("#alert_name").innerHTML = "필수 정보입니다.";
         return false;
     }
     return true;
 }
 
-function checkBirth(form){
-    
+function checkuserYear(form){
+    if(form.userYear.value ==""){
+        document.querySelector("#alert_birth").innerHTML = "태어난 년도 4자리를 정확하게 입력하세요.";
+        return false
+    }
+    return true;
 }
 
+function checkuserMonth(form){
+    console.log(form.userMonth.value);
+    if(form.userMonth.value =="월"){
+        document.querySelector("#alert_birth").innerHTML = "태어난 월을 선택하세요.";
+        return false
+    }
+    return true;
+}
+
+function checkuserDay(form){
+    
+    if(form.userDay.value =="일"){
+        document.querySelector("#alert_birth").innerHTML = "태어난 일(날짜) 을 선택하세요.";;
+        return false
+    }
+    return true;
+}
+ 
 function nextClick(){
+
+    
 
     const form = document.join_form;
     const userId = checkUserId(form);
     const userPassword = checkUserPassword(form);
     const userRepassword = recheckUserPassword(form);
     const userName = checkUserName(form);
-    const userBirth = checkBirth(form);
+    const userDay = checkuserDay(form);
+    const userMonth = checkuserMonth(form);
+    const userYear = checkuserYear(form);
+   
 
     if(userId){
         document.querySelector("#alert_id").innerHTML = "";
@@ -61,15 +88,15 @@ function nextClick(){
     if(userName){
         document.querySelector("#alert_name").innerHTML = "";
     }
+    if(userYear && userMonth && userDay){
+        document.querySelector("#alert_birth").innerHTML = "";
+    }
     
 
 }
 
 function init(){
     currentList();
-
-
-    
 
     const month = document.querySelector("#month");
     for(let i=1; i<=12; i++){
@@ -84,6 +111,8 @@ function init(){
         option.innerText = i;
         day.append(option);
     }
+
+
 
 
     const nextBtn = document.querySelector("#nextButton");
