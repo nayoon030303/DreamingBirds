52f96f3a084fd5122c30f6c2d9e4b973f5ce3b1b
nayoon030303 <nayoon030303@gmail.com>
2021-06-08 08:32:22 +0900

    create home

---
+++ dreaming_birds/js/home.js
@@ -0,0 +1,48 @@
+function MakeCalendar(){
+    const date = new Date();
+
+    const viewYear = date.getFullYear();
+    const viewMonth = date.getMonth();
+
+    document.querySelector('.year-month').textContent = `${viewYear}년 ${viewMonth+1}월`;
+
+    //0을 전달하면, 지난달의 마지막 날의 Date객체가 생성된다.
+    const prevLast = new Date(viewYear,viewMonth,0);
+    const thisLast = new Date(viewYear, viewMonth+1,0);
+
+    const PLDate = prevLast.getDate();
+    const PLDay = prevLast.getDay();
+
+    const TLDate = thisLast.getDate();
+    const TLDay = thisLast.getDay();
+
+    const prevDates = [];
+    //key()메서드는 배열의 각 인덱스를 키 값으로 가지는 새로운 Array Iteraot 객체를 반환
+    //
+    const thisDates = [...Array(TLDate+1).keys()].slice(1);
+    const nextDates = [];
+
+    if(PLDate !== 6){//토요일이 아니라면
+        for(let i=0; i<PLDay+1; i++){
+            prevDates.unshift(PLDate-i);
+        }
+    }
+
+    for(let i=1; i<7-TLDay; i++){
+        nextDates.push(i);
+    }
+
+    const dates = prevDates.concat(thisDates,nextDates);
+
+    dates.forEach((date,i)=>{
+        dates[i] = `<div class = "date">${date}</div>`;
+    })
+
+    document.querySelector('.dates').innerHTML = dates.join('');
+}
+
+function home_init(){
+    MakeCalendar();
+}
+
+window.addEventListener('load',home_init);
