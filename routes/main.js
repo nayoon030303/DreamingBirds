8c28527ad48c70d0395065a6e2241f77f491e352
areum0505 <kimarum2003@gmail.com>
2021-06-14 08:48:32 +0900

    fix homepage

--- routes/main.js
+++ routes/main.js
@@ -16,29 +16,31 @@
 //     });
 // }
 
-var express  = require('express');
-var router   = express.Router();
+var express = require('express');
+var router = express.Router();
 
-router.get('/', function(req,res){
-  if(req && req.session && req.session.count) { 
+router.get('/', function (req, res) {
+  if (req && req.session && req.session.count) {
     // 세션이 존재하는 경우
-      req.session.count = req.session.count + 1;
-      console.log("세션이 존재합니다.")
-  } else { 
+    req.session.count = req.session.count + 1;
+    console.log("세션이 존재합니다.")
+  } else {
     // 세션이 존재하지 않는 경우
-      req.session.count = 1;
-      console.log("세션이 존재하지 않습니다.")
+    req.session.count = 1;
+    console.log("세션이 존재하지 않습니다.")
   }
   console.log(req.session.count);
   res.render('index');
 });
 
 router.get('/study', function (req, res) {
-    res.render('studyPage');
+  res.render('studyPage');
 });
-
-router.get('/googlelogin', function(req,res){
-  res.render('main', {user: req.user});
+router.get('/home', function (req, res) {
+  res.render('homePage');
+});
+router.get('/googlelogin', function (req, res) {
+  res.render('main', { user: req.user });
 });
 
 module.exports = router;
